9fe189b754b83ff28368e3ff7fdead9ed479d447
USER <yarmiztechnlogies@gmail.com>
2022-09-27 14:00:34 +0530

    <MounikEnzapps>

---
+++ __manifest__.py
@@ -0,0 +1,18 @@
+{
+    "name": "Natcom Credit Report",
+    "version": "14.0",
+    "category": "Accounting",
+    "license": "",
+    "summary": "Natcom Credit Report",
+    "author": "Enzapps Private Limited",
+    "website": "https://www.enzapps.com",
+    "depends": ["base","account"],
+    "data": [
+        'report/reports_header_new_view.xml',
+        'report/report.xml',
+        'report/reports_no_new_view.xml',
+    ],
+    "qweb": [
+    ],
+    "installable": True,
+}
